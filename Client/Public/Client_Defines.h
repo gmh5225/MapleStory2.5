2c51a61a60ee872541ef7f02991c12901da516f3
Ahnhyunsoo <86905150+Ahnhyunsoo@users.noreply.github.com>
2022-08-04 21:35:42 +0900

    zzz
    zz

--- Client/Public/Client_Defines.h
+++ Client/Public/Client_Defines.h
@@ -3,6 +3,8 @@
 namespace Client
 {
 #pragma warning (disable : 4244)
+#pragma warning (disable : 4305)
+#pragma warning (disable : 4309)
 	static const unsigned int g_iWinSizeX = 1280;
 	static const unsigned int g_iWinSizeY = 720;
 	enum LEVEL { LEVEL_STATIC, LEVEL_LOADING, LEVEL_LOGO, LEVEL_GAMEPLAY, LEVEL_HENESYS, LEVEL_END };
