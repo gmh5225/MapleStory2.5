76d3daebd2e917b561d2c776798cfa8f4e93436b
Ahnhyunsoo <86905150+Ahnhyunsoo@users.noreply.github.com>
2022-08-15 10:46:18 +0900

    12
    12

--- Client/Private/BlackWizardFinal.cpp
+++ Client/Private/BlackWizardFinal.cpp
@@ -87,7 +87,7 @@ void CBlackWizardFinal::LateTick(_float fTimeDelta)
 	
 		
 	Compute_CamDistance(m_pTransformCom->Get_State(CTransform::STATE_POSITION));
-	m_pRendererCom->Add_RenderGroup(CRenderer::RENDER_ALPHABLEND, this);
+	m_pRendererCom->Add_RenderGroup(CRenderer::RENDER_BOSS, this);
 
 	m_pColliderCom->Add_BoxCollsionGroup(CCollider::COLLSION_MONSTER_SKILL, this);
 
